4c917db405b34d10f26d436e9ef00788aab993d6
amanseenha <2105521@kiit.ac.in>
2023-02-23 12:51:20 +0530

    minute changes

--- Lab 4/Q4.java
+++ Lab 4/Q4.java
@@ -5,14 +5,13 @@ public class Q4 {
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter number: ");
     int num = sc.nextInt();
-    int newNum = 0;
     int rev = 0, n = num;
     while (n > 0) {
-      int r = n % 10;
-      rev = rev * 10 + r;
+      int rem = n % 10;
+      rev = rev * 10 + rem;
       n /= 10;
     }
-    if (newNum == n) {
+    if (rev == num) {
       System.out.println("Palindrome Successfully detected");
     } else System.out.println("The input number is not a palindrome");
     sc.close();
---
+++ Lab 5/Lab_5_Q_1.java
@@ -0,0 +1,5 @@
+public class Lab_5_Q_1 {
+    public static void main(String[] args) {
+       
+    }    
+}
