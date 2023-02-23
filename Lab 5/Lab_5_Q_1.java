9aac42085fdd92e3d868eebf36cb33045946b55b
amanseenha <2105521@kiit.ac.in>
2023-02-23 13:35:07 +0530

    Lab-

--- Lab 5/Lab_5_Q_1.java
+++
@@ -1,5 +0,0 @@
-public class Lab_5_Q_1 {
-    public static void main(String[] args) {
-       
-    }    
-}
---
+++ Lab-5/Lab_5_Q1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Lab_5_Q1 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter numbers: ");
+       for (int i = 0; i < 3; i++) {
+            
+       }
+    }    
+}
