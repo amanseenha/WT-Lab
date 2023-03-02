dfc950d3704ea4f396a275061055f51f38dda91d
amanseenha <2105521@kiit.ac.in>
2023-03-02 10:14:12 +0530

    Lab 5

--- Lab-5/Lab_5_Q1.java
+++ Lab-5/Lab_5_Q1.java
@@ -1,10 +1,23 @@
-import java.util.Scanner;
 public class Lab_5_Q1 {
-    public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       System.out.print("Enter numbers: ");
-       for (int i = 0; i < 3; i++) {
-            
-       }
-    }    
+
+  static int largest(int arr[]) {
+    int max = arr[0];
+    for (int i = 1; i < 3; i++) {
+      if (arr[i] > max) max = arr[i];
+    }
+    return max;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Enter 3 numbers : ");
+    int arr[] = new int[3];
+    for (int i = 0; i < 3; i++) {
+      try {
+        arr[i] = Integer.parseInt(args[i]);
+      } catch (NumberFormatException e) {
+        System.out.println("!!Please enter valid integers!!");
+      }
+    }
+    System.out.println("Largest among entered 3 numbers is : " + largest(arr));
+  }
 }
---
+++ Lab-5/Lab_5_Q6.java
@@ -0,0 +1,17 @@
+class objectCreator{
+	static int objCounter=0;
+	objectCreator(){
+		objCounter++;
+	}
+}
+
+public class Lab_5_Q6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		objectCreator s1=new objectCreator();
+		objectCreator s2=new objectCreator();
+		objectCreator s3=new objectCreator();
+		System.out.println(objectCreator.objCounter);
+	}
+}
